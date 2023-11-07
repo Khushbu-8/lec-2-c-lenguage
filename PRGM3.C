921ab9632c6efde879609b48e828eb3195610f24
khushbu-8 <khushbuzalavadiya8@gmail.com>
2023-11-07 15:51:05 +0530

    lec-2

---
+++ PRGM3.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int a;
+int b;
+int c;
+	clrscr();
+
+    a = 12;
+    b = 6;
+    c = a * b;
+
+    printf("multiplication of %d and %d is %d",a,b,c);
+
+
+
+
+
+getch();
+}
---
+++ PRGM4.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+int a;
+int b;
+int c;
+       clrscr();
+     a = 12;
+     b = 6;
+     c = a / b;
+
+     printf("modulo of %d and %d is %d",a,b,c);
+     getch();
+     }
